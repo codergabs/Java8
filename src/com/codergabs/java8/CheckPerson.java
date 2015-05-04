0c94f5939b7bf5c214664b4b86e8af223968d967
codergabs <gourab.cst2006@gmail.com>
2015-05-04 14:39:56 +0530

    ok

--- src/com/codergabs/java8/CheckPerson.java
+++ src/com/codergabs/java8/CheckPerson.java
@@ -1,6 +1,14 @@
 package com.codergabs.java8;
 
+@FunctionalInterface
 public interface CheckPerson {
+	// one abstract method
 	boolean test(Person p);
-
+	//default method
+	default boolean defaulttest(Person p){
+		if (p==null){
+			return false;
+		}
+		return true;
+	}		
 }
